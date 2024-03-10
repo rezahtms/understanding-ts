efd9821a9b0144da6eda4b905b5e1bbc6ab1870d
rezahatampoor <rezahtms@gmail.com>
2024-03-10 20:54:07 +0330

    working with number,boolean and string types

--- app.ts
+++ app.ts
@@ -8,11 +8,18 @@
 
 // console.log(addJs("2", 3));
 
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 5;
 const number2 = 5;
-const result = add(number1, number2);
-console.log(result);
+const showResult = true;
+const phase = "Result is ";
+// const result = add(number1, number2, showResult);
+add(number1, number2, showResult, phase);
