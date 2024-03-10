cefaecd364bce508bac381b82e881e37386e02c5
rezahatampoor <rezahtms@gmail.com>
2024-03-10 20:21:42 +0330

    diffrent between ts vs js

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
+// function addJs(n1, n2) {
+//   if (typeof n1 === "number" && typeof n2 === "number") {
+//     return n1 + n2;
+//   } else {
+//     throw new Error("Incorrect input1");
+//   }
+// }
+
+// console.log(addJs("2", 3));
+
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
 const number1 = 5;
-const number2 = 2.8;
+const number2 = 5;
 const result = add(number1, number2);
 console.log(result);
